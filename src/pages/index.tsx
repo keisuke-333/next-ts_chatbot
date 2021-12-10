366ddb9660f61940794309c6f175f1706a7134d9
keisuke-333 <keikubo.999@gmail.com>
2021-12-10 21:39:23 +0900

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>チャットボット</title>
+        <meta name="description" content="チャットボットアプリ" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <p>テスト</p>
+    </div>
+  )
+}
+
+export default Home
