9f8debddc522ce68baf38437b8c9ec94291cee8e
keisuke-333 <keikubo.999@gmail.com>
2021-12-21 00:34:40 +0900

    list apiを型定義

--- src/pages/api/history/list.ts
+++ src/pages/api/history/list.ts
@@ -1,7 +1,8 @@
 import { NextApiHandler } from 'next'
+import type { Post } from '@prisma/client'
 import prisma from '../../../lib/prisma'
 
-const list: NextApiHandler = async (req, res) => {
+const list: NextApiHandler<Array<Post>> = async (req, res) => {
   if (req.method !== 'GET') res.status(405).end()
 
   const postList = await prisma.post.findMany({
